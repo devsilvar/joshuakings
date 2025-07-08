06c7a3fee18077e85092f246e7a2843d525a8711
refsilva <refsilva@yahoo.com>
2025-07-08 11:45:31 +0100

    fixed proptypes issues and cls issues

--- src/assets/components/homepage/Maplocation.tsx
+++ src/assets/components/homepage/Maplocation.tsx
@@ -23,7 +23,7 @@ const locations = [
     name: 'Chevron',
     properties: 10,
     image: '/chevron.png',
-    position: 'top-[50%] left-[10%]',
+    position: 'top-[50%] lg:left-[10%] left-[17%]',
   },
   {
     name: 'Ajao',
@@ -41,7 +41,7 @@ const locations = [
     name: 'Yaba',
     properties: 10,
     image: '/yaba.png',
-    position: 'top-[50%] left-[90%]',
+    position: 'top-[50%] lg:left-[90%] left-[85%]',
   },
 ];
 
@@ -61,7 +61,7 @@ const MapSection = () => {
 
   return (
     <div className='relative w-full bg-white py-16'>
-      <h2 className='text-center text-[28px] font-[700] md:text-3xl w-[50%] mx-auto mb-6'>
+      <h2 className='text-center text-[18px] font-[700] md:text-3xl lg:w-[50%] mx-auto mb-6'>
         We’ve handpicked the best spots in Lagos so you don’t have to stress
       </h2>
 
@@ -71,19 +71,25 @@ const MapSection = () => {
             key={idx}
             className={`group absolute ${loc.position} transform -translate-x-1/2 -translate-y-1/2 text-center`}
           >
-            <div className='relative'>
+            <div className='relative text-center'>
               <img
                 src={loc.image}
                 alt={loc.name}
-                className='w-[60px] h-[60px] rounded-full border-4 border-white shadow-md object-cover transition-transform duration-300 group-hover:scale-110'
+                className='lg:w-[60px] mx-auto  w-[51px] h-[51px] lg:h-[60px] rounded-full border-4 border-white shadow-md object-cover transition-transform duration-300 group-hover:scale-110'
               />
               {/* Tooltip */}
               <div className='absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-black text-white text-xs rounded px-2 py-1 shadow-lg'>
                 {loc.name} – {loc.properties} Properties
               </div>
             </div>
-            <p className='text-sm font-semibold mt-1 text-black'>{loc.name}</p>
-            <p className='text-xs text-gray-600'>{loc.properties} Properties</p>
+            <div className=''>
+              <p className='text-sm font-semibold mt-1 text-black'>
+                {loc.name}
+              </p>
+              <p className='text-xs text-gray-600'>
+                {loc.properties} Properties
+              </p>
+            </div>
           </div>
         ))}
       </div>
